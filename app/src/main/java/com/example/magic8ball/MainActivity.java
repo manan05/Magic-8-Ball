9b86e841c93993585c1bbb1c36f6fb82bbcb8e3f
Manan Arora <31929934+manan0313@users.noreply.github.com>
2019-08-30 20:40:03 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/magic8ball/MainActivity.java
+++ app/src/main/java/com/example/magic8ball/MainActivity.java
@@ -3,13 +3,24 @@ package com.example.magic8ball;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
-    ImageView imgBtn;
+    ImageButton imgBtn;
     TextView tvAnswer;
+
+
+    public String[] answer =
+            {"It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes",
+                    "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later",
+                    "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it",
+                    "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +28,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         imgBtn = findViewById(R.id.imgBtn);
         tvAnswer = findViewById(R.id.tvAnswer);
+
+        imgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    int randomNumber;
+                    Random rand = new Random();
+                    randomNumber = rand.nextInt(answer.length);
+                    tvAnswer.setText(answer[rand.nextInt(randomNumber)]);
+            }
+        });
     }
 }

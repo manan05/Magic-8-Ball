049462f1f9ca9c4e4859fbe21bb730ad9bd94838
Manan Arora <31929934+manan05@users.noreply.github.com>
2019-10-18 22:12:15 +0530

    Sample Images added

--- app/src/main/java/com/example/magic8ball/MainActivity.java
+++ app/src/main/java/com/example/magic8ball/MainActivity.java
@@ -18,7 +18,8 @@ public class MainActivity extends AppCompatActivity {
 
     public String[] answer =
             {"Don't count on it", "My reply is no", "My sources say no",
-                    "Outlook not so good", "Very doubtful", "It is certain", "It is decidedly so", "Without a doubt", "Yes definitely",
+                    "Outlook not so good", "Very doubtful", "It is certain", "It is decidedly so",
+                    "Without a doubt", "Yes definitely",
                     "You may rely on it", "As I see it, yes", "Most likely", "Yes",
                     "Signs point to yes", "Reply hazy try again", "Ask again later",
                     "Better not tell you now", "Cannot predict now", "Concentrate and ask again"

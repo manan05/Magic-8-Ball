c9567b4b62f1e7b90338ebc9145e4f4daed12dd5
Manan Arora <31929934+manan05@users.noreply.github.com>
2019-10-18 21:53:02 +0530

    Added Icon to app

--- app/src/main/java/com/example/magic8ball/MainActivity.java
+++ app/src/main/java/com/example/magic8ball/MainActivity.java
@@ -1,12 +1,12 @@
 package com.example.magic8ball;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Random;
 
@@ -17,10 +17,13 @@ public class MainActivity extends AppCompatActivity {
 
 
     public String[] answer =
-            {"It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes",
-                    "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later",
-                    "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it",
-                    "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"};
+            {"Don't count on it", "My reply is no", "My sources say no",
+                    "Outlook not so good", "Very doubtful", "It is certain", "It is decidedly so", "Without a doubt", "Yes definitely",
+                    "You may rely on it", "As I see it, yes", "Most likely", "Yes",
+                    "Signs point to yes", "Reply hazy try again", "Ask again later",
+                    "Better not tell you now", "Cannot predict now", "Concentrate and ask again"
+            };
+    public static final String TAG = "Lame";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,13 +35,14 @@ public class MainActivity extends AppCompatActivity {
         imgBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                while (true) {
-                    int randomNumber;
-                    Random rand = new Random();
-                    randomNumber = rand.nextInt(answer.length);
-                    tvAnswer.setText(answer[rand.nextInt(randomNumber)]);
-                    break;
-                }
+
+                int randomNumber;
+                Random rand = new Random();
+                randomNumber = rand.nextInt(answer.length);
+                tvAnswer.setText(answer[rand.nextInt(randomNumber)]);
+                Log.d(TAG, "onClick:ded " + randomNumber);
+//                    break;
+
             }
         });
     }
